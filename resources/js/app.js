4ccb69ac1d654d256d7a762570c0bbdcf56db51c
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-06-13 17:48:55 +0300

    creating point of entering for backend and for frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import Vue from "vue";
+import Index from "./components";
+import router from "./router";
+
+require('./bootstrap');
+
+new Vue({
+    el: '#app',
+
+    components: {
+        Index
+    },
+
+    router
+})
